305f9363007f9e2e1d6bbd9e40fd03c20b7322b9
Jungho Kwon <jango@jango-centos.lan>
2021-11-05 22:19:31 -0500

    Initial commit

---
+++ src/main/java/life/lamis/alveolaauthservice/AlveolaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package life.lamis.alveolaauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlveolaAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlveolaAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/life/lamis/alveolaauthservice/AlveolaAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package life.lamis.alveolaauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlveolaAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
